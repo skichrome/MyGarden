bda0e283b710e1491173abd669d04b8776a69278
skichrome <toni.campeol@outlook.fr>
2025-01-06 06:06:18 +0100

    Correction pour build Jenkins.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,13 +86,7 @@ android {
             isDebuggable = true
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+
     buildFeatures {
         dataBinding = true
         buildConfig = true
